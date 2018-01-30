b75e54d35a32294f75dacd0188b4b66cb2c0991f
Nisvet Mujkic <nisvet.mujkic@edu.fit.ba>
2018-01-30 11:02:48 +0100

    Console App

---
+++ Understanding Internationlization/Understanding Internationlization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Understanding_Internationlization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
