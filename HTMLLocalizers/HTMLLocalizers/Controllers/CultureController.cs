1154bcb3424fc7b718b40dd552700b3ff9372a83
Nisvet Mujkic <nisvet.mujkic@edu.fit.ba>
2018-01-31 16:15:50 +0100

    s

---
+++ HTMLLocalizers/HTMLLocalizers/Controllers/CultureController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLLocalizers.Controllers
+{
+    public class CultureController : Controller
+    {
+        [HttpPost]
+        public IActionResult Set(string uiCulture, string returnUrl)
+        {
+            IRequestCultureFeature feature = HttpContext.Features.Get<IRequestCultureFeature>();
+
+            RequestCulture request = new RequestCulture(feature.RequestCulture.Culture, new CultureInfo(uiCulture));
+
+            string cookieValue = CookieRequestCultureProvider.MakeCookieValue(request);
+            string cookieName = CookieRequestCultureProvider.DefaultCookieName;
+
+            Response.Cookies.Append(cookieName, cookieValue);
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ HTMLLocalizers/HTMLLocalizers/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace HTMLLocalizers
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IMvcBuilder mvcBuilder = services.AddMvc();
+
+            mvcBuilder.AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
+            services.Configure<LocalizationOptions>(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.SupportedUICultures = new List<CultureInfo>()
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("hr-HR"),
+                    new CultureInfo("se-SE")
+                };
+
+                options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US");
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            
+            app.UseRequestLocalization();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
